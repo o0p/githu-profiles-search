1d7f7cd8e5602c18e8325c344ca86e254d72a5b8
Russell <russelldev@protonmail.com>
2021-01-31 17:59:31 +0000

    Add axios to js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const APIURL = 'https://api.github.com/users/';
 
+getUser('o0p');
+
+
+async function getUser(username) {
+    try {
+        const { data } = await axios(APIURL + username);
+        console.log(data);
+    } catch {
+        console.log(error)
+    }
+
+}
