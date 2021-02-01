013ba608c75bd5eaa853fab1638726fdbe142030
Russell <russelldev@protonmail.com>
2021-02-01 06:44:18 +0000

    Add repos to card funct

--- script.js
+++ script.js
@@ -8,12 +8,23 @@ async function getUser(username) {
     try {
         const { data } = await axios(APIURL + username);
         createUserCard(data);
+        getRepos(username)
     } catch(err) {
       if(err.response.status == 404) {
         createErrorCard('No profile with this username');
       }
     }
 };
+
+async function getRepos(username) {
+    try {
+      const { data } = await axios(APIURL + username + '/repos');
+      addReposToCard(data);
+  } catch(err) {
+      createErrorCard('Problim fetching repos');
+    }
+}
+
 
 function createUserCard(user) {
     const cardHTML = `
