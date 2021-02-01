bc00488fc5f36f872cfe241dde8ec743a0b6c493
Russell <russelldev@protonmail.com>
2021-02-01 06:51:15 +0000

    Add repos fetching

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
-
 async function getUser(username) {
     try {
         const { data } = await axios(APIURL + username);
@@ -18,12 +17,12 @@ async function getUser(username) {
 
 async function getRepos(username) {
     try {
-      const { data } = await axios(APIURL + username + '/repos');
+      const { data } = await axios(APIURL + username + '/repos?sort=created');
       addReposToCard(data);
   } catch(err) {
       createErrorCard('Problim fetching repos');
     }
-}
+};
 
 
 function createUserCard(user) {
@@ -54,9 +53,24 @@ function createErrorCard(msg) {
         <h1>${msg}</h1>
     </div>
   `
-
   main.innerHTML = cardHTML;
 }
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById('repos')
+
+  repos
+      .slice(0, 5)
+      .forEach(repo => {
+          const repoEl = document.createElement('a');
+          repoEl.classList.add('repo');
+          repoEl.href = repo.html_url;
+          repoEl.target = '_blank';
+          repoEl.innerText = repo.name;
+
+          reposEl.appendChild(repoEl);
+      });
+};
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
